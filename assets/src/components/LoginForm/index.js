b61fc8af3e6bcd9ba21b7991f54ea8f95c3f38b4
psmigielski <walek.smigielski@gmail.com>
2021-05-02 21:49:26 +0200

    add animations to home page & validation to forms

--- assets/src/components/LoginForm/index.js
+++ assets/src/components/LoginForm/index.js
@@ -1,21 +1,47 @@
 import { useFormik } from "formik";
-import React from "react";
+import React, {useRef} from "react";
 import { Link } from "react-router-dom";
 import RegisterModal from "../RegisterModal";
+import gsap from "gsap"
 
 const LoginForm = () =>{
     const [isOpen, setIsOpen] = React.useState(false);
-    const handleLogin = (values) => {
-        //add login handling
+    const registerModalRef = useRef(null);
+    const registerModalBackgroundRef = useRef(null);
+    const handleLogin = () => {
+        //xD
+    }
+    const validate = (values) => {
+        let errors = {}
+        if(!values.email){
+            errors.email = "pole jest wymagane"
+        }
+        if(!values.password){
+            errors.password = "pole jest wymagane"
+        }
+        return errors;
+    }
+    const handleModalOpen = () => {
+        setIsOpen(true);
+        setTimeout(()=>{
+            const tl = gsap.timeline({defaults: {ease: 'power3.out'}});
+            tl.fromTo(registerModalRef.current, {opacity: 0}, {opacity:1,visibility:"visible", duration:.5})
+            .fromTo(registerModalBackgroundRef.current,{opacity: 0}, {opacity:1,visibility:"visible", duration:.5},"-=0.5" );
+        }, 100)
+    }
+    const handleModalClose = () => {
+        const tl = gsap.timeline({defaults: {ease: 'power3.out'}});
+        tl.fromTo(registerModalRef.current, {opacity: 1}, {opacity:0,duration:.5, onComplete:setIsOpen, onCompleteParams:[false]})
+        .fromTo(registerModalBackgroundRef.current,{opacity: 1}, {opacity:0,duration:.5},"-=0.5" );
     }
     const formik = useFormik({
         initialValues:{
             email: '',
             password: ''
         },
+        validate,
         onSubmit: handleLogin
     });
-
     return (
         <div className="loginFromWrapper">
             <form className="loginForm" onSubmit={formik.handleSubmit}>
@@ -30,7 +56,7 @@ const LoginForm = () =>{
                             value={formik.values.email}
                             placeholder="Email"
                         />
-                        <p className="error"></p>
+                        <p className="error">{formik.touched.email && formik.errors.email ? formik.errors.email : null}</p>
                     </div>
                     <div className="inputContainer">
                         <input 
@@ -42,7 +68,7 @@ const LoginForm = () =>{
                             value={formik.values.password}
                             placeholder="Hasło"
                         />
-                        <p className="error"></p>
+                        <p className="error">{formik.touched.password && formik.errors.password ? formik.errors.password : null}</p>
                     </div>
                 </div>
                 <div className="buttonContainer">
@@ -50,10 +76,10 @@ const LoginForm = () =>{
                         Zaloguj się
                     </button>
                     <div className="buttonContainerSeparator"></div>
-                    <button className="registerButton" onClick={() => setIsOpen(true)}>
+                    <button className="registerButton" type="button" onClick={handleModalOpen}>
                         Zarejestruj się
                     </button>
-                    <RegisterModal open={isOpen} onClose={() => setIsOpen(false)}/>
+                    <RegisterModal ref={[registerModalRef, registerModalBackgroundRef]} open={isOpen} onClose={handleModalClose}/>
                 </div>
                 <p className="loginFormLabel">Zapomniałeś hasło? <Link to="/forget">kliknij tutaj</Link></p>
             </form>
--- assets/src/components/RegisterModal/index.js
+++ assets/src/components/RegisterModal/index.js
@@ -1,25 +1,37 @@
-import React, {useState} from 'react'
+import React, {useRef, useState} from 'react'
 import ReactDOM from "react-dom";
+import gsap from 'gsap';
 import CloseSVG from '../CloseSVG';
 import RegisterFirstStep from '../RegisterFirstStep';
 import RegisterSecondStep from '../RegisterSecondStep';
 
-export default function RegisterModal({open, onClose}) {
+const RegisterModal = React.forwardRef(({open, onClose}, ref) => {
     if(!open) return null;
-    const [currentStep, setCurrentStep] = useState(1);
     const [firstStepData, setFirstStepData] = useState({});
-    const [secondtStepData, setSecondStepData] = useState({});
-
+    const handleSubmitEvent = (secondStepData) =>{
+        console.log({...firstStepData,...secondStepData});
+    }
+    const firstStepRef = useRef(null);
+    const secondStepRef = useRef(null);
+    const circleRef = useRef(null);
+    const initalStyle = {
+        opacity: 0,
+        visibility:"hidden",
+    }
     const prevStep = () => {
-        setCurrentStep(currentStep <= 1? 1: currentStep - 1)
+        const tl = gsap.timeline({defaults: {ease: 'power3.out'}})
+        tl.fromTo(secondStepRef.current, {xPercent:0, opacity:1, duration:1}, {xPercent:100, opacity:0, display:'none', duration:1},'-=0.0001')
+        .fromTo(firstStepRef.current, {xPercent:-100, opacity:0}, {xPercent:0,opacity:1,display:'flex', duration: 1},'-=0.0001').to(circleRef.current, {fill:'#C4C4C4'},"-=2")
     }
     const nextStep = () => {
-        setCurrentStep(currentStep == 2? 2: currentStep + 1);
+        const tl = gsap.timeline({defaults: {ease: 'power3.out'}})
+        tl.fromTo(firstStepRef.current, {xPercent:0, opacity:1, duration:1}, {xPercent:-100,opacity:0,display:'none', duration: 1}, '-=0.0001')
+        .fromTo(secondStepRef.current, {xPercent:100, opacity:0}, {xPercent:0, opacity:1, display:'flex', duration:1}, '-=0.0001').to(circleRef.current, {fill:'#467AFF'},"-=2")
     }
     return ReactDOM.createPortal(
         <>
-            <div className="registerModalOverlay" />
-            <div className="registerModal">
+            <div className="registerModalOverlay" ref={ref[1]} style={initalStyle} />
+            <div className="registerModal" ref={ref[0]} style={initalStyle}>
                 {/* 10 20 60 10 */}
                 <div className="registerFormWrapper">
                     <div className="registerModalHeader">
@@ -38,17 +50,16 @@ export default function RegisterModal({open, onClose}) {
                         <div className="registerModalNavEl">
                             <p className="registerModalNavElParagraph">Dane Konta</p>
                             <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <circle cx="20" cy="20" r="20" fill={currentStep ==2 ? "#467AFF" : "#C4C4C4"}/>
+                                <circle cx="20" cy="20" r="20" fill="#C4C4C4" ref={circleRef}/>
                             </svg>
                         </div>
-                    </nav>
-                    {currentStep == 1 ? 
-                        <RegisterFirstStep nextStep={nextStep} setFirstStepData={setFirstStepData} currentStep={currentStep}/> :
-                        <RegisterSecondStep prevStep={prevStep} setSecondStepData={setSecondStepData} currentStep={currentStep}/>
-                    }
+                    </nav> 
+                        <RegisterFirstStep ref={firstStepRef} nextStep={nextStep} setFirstStepData={setFirstStepData} /> 
+                        <RegisterSecondStep ref={secondStepRef} prevStep={prevStep} handleSubmitEvent={handleSubmitEvent} />
                     
                    
                 </div>
             </div>
         </>, document.getElementById('portal'));
-}
+});
+export default RegisterModal;
