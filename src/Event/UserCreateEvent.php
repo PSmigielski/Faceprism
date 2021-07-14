399a5fb95d5fd43db519ac57fa870ca365ce3fee
PSmigielski <walek.smigielski@gmail.com>
2021-07-14 16:21:31 +0200

    add user create event

--- src/Event/UserCreateEvent.php
+++ src/Event/UserCreateEvent.php
@@ -2,22 +2,31 @@
     namespace App\Event;
 
     use App\Entity\User;
+    use Symfony\Component\HttpFoundation\Response;
     use Symfony\Contracts\EventDispatcher\Event;
-
+    
     class UserCreateEvent extends Event{
-        public const NAME = "user.delete";
+        public const NAME = "user.create";
         protected User $user;
-
-        public function __construct(User $user)
+        protected Response $response;
+        public function __construct(User $user, Response $response)
         {
             $this->user = $user;
+            $this->response = $response;
         }
 
         public function getUser(): User
         {
             return $this->user;
         }
-
+        public function getResponse(): Response
+        {
+            return $this->response;
+        }
+        public function setResponse(Response $response): void
+        {
+            $this->response = $response;
+        }
         public function __toString(): string
         {
             return sprintf('User ID: %s', $this->user->getId());
