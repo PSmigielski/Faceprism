4059bd463cb28f655ec695273719c08b7cd9d226
PSmigielski <walek.smigielski@gmail.com>
2021-07-15 16:13:05 +0200

    add new valid content type toa api csrf validation

--- src/EventSubscriber/ApiCsrfValidationSubscriber.php
+++ src/EventSubscriber/ApiCsrfValidationSubscriber.php
@@ -21,7 +21,7 @@ class ApiCsrfValidationSubscriber implements EventSubscriberInterface
         if (!$request->attributes->get('_is_api')) {
             return;
         }
-        if ($request->headers->get('Content-Type') != 'application/json') {
+        if ($request->headers->get('Content-Type') == 'application/json' || strpos($request->headers->get('Content-Type'), 'multipart/form-data') != false ) {
             $response = new JsonResponse([
                 'message' => 'Invalid Content-Type'
             ], 415);
