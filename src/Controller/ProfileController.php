15fdec06105ee462de232aa263d5fef0f3bd6f81
PSmigielski <walek.smigielski@gmail.com>
2021-07-16 12:31:00 +0200

    add entpoint for updating banner

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -15,9 +15,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProfileController extends AbstractController
 {
     /**
-     * @Route("/addprofilepic", name="change_profilepic",methods={"POST"})
+     * @Route("/profilepic", name="change_profile_pic",methods={"POST"})
      */
-    public function index(Request $req, ImageUploader $imageUploader): JsonResponse
+    public function updateProfilePic(Request $req, ImageUploader $imageUploader): JsonResponse
     {
         $em = $this->getDoctrine()->getManager();
         $userID = $req->request->get("userID");
@@ -26,10 +26,31 @@ class ProfileController extends AbstractController
             return new JsonResponse(["error"=>"user with this id does not exist!"], 404);
         }else{
             if(strpos($req->files->get("image")->getMimeType(), 'image') !== false){ 
-                $user->setProfilePicUrl($imageUploader->uploadFileToCloudinary($req->files->get("image")));
+                $user->setProfilePicUrl($imageUploader->uploadFileToCloudinary($req->files->get("image"), 200 , 200,"profile_pic"));
                 $em->persist($user);
                 $em->flush();
-                return new JsonResponse(["message" => "profile picture has been added"], 201);
+                return new JsonResponse(["message" => "profile picture has been updated"], 201);
+            } else{
+                return new JsonResponse(["error" => "wrong file format"], 415);
+            }
+        }
+    }
+    /**
+     * @Route("/banner", name="change_banner",methods={"POST"})
+     */
+    public function updateBanner(Request $req, ImageUploader $imageUploader): JsonResponse
+    {
+        $em = $this->getDoctrine()->getManager();
+        $userID = $req->request->get("userID");
+        $user = $em->getRepository(User::class)->find($userID);
+        if(is_null($user)){
+            return new JsonResponse(["error"=>"user with this id does not exist!"], 404);
+        }else{
+            if(strpos($req->files->get("image")->getMimeType(), 'image') !== false){ 
+                $user->setBannerUrl($imageUploader->uploadFileToCloudinary($req->files->get("image"), 820 , 312, "banner"));
+                $em->persist($user);
+                $em->flush();
+                return new JsonResponse(["message" => "banner has been updated"], 201);
             } else{
                 return new JsonResponse(["error" => "wrong file format"], 415);
             }
