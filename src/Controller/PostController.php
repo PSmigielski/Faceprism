f6549f8bd2527b7dd9c05691c6f812ee15d1f126
psmigielski <walek.smigielski@gmail.com>
2021-10-09 21:56:53 +0200

    add creating post on pages and get posts for user and page

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Like;
+use App\Entity\Page;
 use App\Entity\Post;
 use App\Entity\User;
 use App\Repository\PostRepository;
@@ -30,21 +31,45 @@ class PostController extends AbstractController
      */
     public function index(PostRepository $repo, SerializerInterface $serializer, Request $request): JsonResponse
     {
+        $userID = $request->query->get("u", null);
+        $pageID = $request->query->get("p", null);
         $payload = $request->attributes->get("payload");
         $page = $request->query->get('page', 1);
-        $qb = $repo->createFindAllQuery(UUIDService::encodeUUID($payload["user_id"]));
-        $data = PaginationService::paginate($page,$qb,"posts");
-        $em = $this->getDoctrine()->getManager();
-        if(gettype($data)=="array"){
+        if (is_null($pageID) && is_null($userID)) {
+            $qb = $repo->createFindAllQuery(UUIDService::encodeUUID($payload["user_id"]));
+            $data = PaginationService::paginate($page, $qb, "posts");
+        } else {
+            if (!is_null($pageID)) {
+                $pageEntity = $this->getDoctrine()->getRepository(Page::class)->find(UUIDService::encodeUUID($pageID));
+                if (!$pageEntity) {
+                    return new JsonResponse(["error" => "page with this ID does not exist!"], 404);
+                }
+                $qb = $repo->createFindAllPostsForPage(UUIDService::encodeUUID($pageID));
+                $data = PaginationService::paginate($page, $qb, "posts");
+            }
+            if (!is_null($userID)) {
+                $user = $this->getDoctrine()->getRepository(User::class)->find(UUIDService::encodeUUID($userID));
+                if (!$user) {
+                    return new JsonResponse(["error" => "user with this ID does not exist!"], 404);
+                }
+                $qb = $repo->createFindAllPostsForUser(UUIDService::encodeUUID($userID));
+                $data = PaginationService::paginate($page, $qb, "posts");
+            }
+        }
+        if (gettype($data) == "array") {
             $serializer = new Serializer([new ObjectNormalizer()], [new JsonEncoder()]);
-            $resData = $serializer->serialize($data, "json",['ignored_attributes' => ['posts', "transitions", "timezone", "password", "email", "username","roles","gender", "salt", "post"]]);
+            $resData = $serializer->serialize($data, "json", ['ignored_attributes' => ['posts', "transitions", "timezone", "password", "email", "username", "roles", "gender", "salt", "post", "website", "bannerUrl", "bio", "verified", "dateOfBirth", "poComments"]]);
             $tmp = json_decode($resData, true);
             $tmpPosts = [];
-            foreach($tmp["posts"] as $p){
-                $like = $em->getRepository(Like::class)->findBy(["li_post"=>$p["id"], "li_user"=>UUIDService::encodeUUID($payload["user_id"])]);
+            foreach ($tmp["posts"] as $p) {
+                $like = $this->getDoctrine()->getRepository(Like::class)->findBy(["li_post" => $p["id"], "li_user" => UUIDService::encodeUUID($payload["user_id"])]);
                 !empty($like) ? $p["isLiked"] = true : $p["isLiked"] = false;
                 $p["id"] = UUIDService::decodeUUID($p["id"]);
                 $p["author"]["id"] = UUIDService::decodeUUID($p["author"]["id"]);
+                if (!is_null($p["page"])) {
+                    $p["page"]["id"] = UUIDService::decodeUUID($p["page"]["id"]);
+                    unset($p["page"]["owner"]);
+                }
                 array_push($tmpPosts, $p);
             }
             $tmp["posts"] = $tmpPosts;
@@ -59,42 +84,54 @@ class PostController extends AbstractController
     public function show(string $id, SerializerInterface $serializer): JsonResponse
     {
         $post = $this->getDoctrine()->getRepository(Post::class)->find(UUIDService::encodeUUID($id));
-        if(!$post){
-            return new JsonResponse(["message" => "Post with this id does not exist"],404);
+        if (!$post) {
+            return new JsonResponse(["error" => "post with this id does not exist"], 404);
         }
-        $resData = $serializer->serialize($post, "json",['ignored_attributes' => ['posts', "transitions", "timezone", "password", "email", "username","roles","gender", "salt", "post"]]);
-        $tmp=json_decode($resData, true);
+        $resData = $serializer->serialize($post, "json", ['ignored_attributes' => ['posts', "transitions", "timezone", "password", "email", "username", "roles", "gender", "salt", "post"]]);
+        $tmp = json_decode($resData, true);
         $tmp["id"] = UUIDService::decodeUUID($tmp["id"]);
         return new JsonResponse($tmp, 200);
     }
     /**
      * @Route("", name="add_post", methods={"POST"})
      */
-    public function create(Request $request, ImageUploader $imageUploader):JsonResponse
+    public function create(Request $request, ImageUploader $imageUploader): JsonResponse
     {
+        $pageID = $request->query->get("p", null);
         $payload = $request->attributes->get("payload");
         $post = new Post();
         $author = $this->getDoctrine()->getRepository(User::class)->find(UUIDService::encodeUUID($payload["user_id"]));
-        if(!$author){
-            return new JsonResponse(["error"=> "user with this id does not exist!"], 404);
+        if (!$author) {
+            return new JsonResponse(["error" => "user with this id does not exist!"], 404);
         }
-        if($request->request->get("text") != "" && !is_null($request->request->get("text"))){
-            $post->setText($request->request->get("text"));
-        }else{
-            return new JsonResponse(["error"=>"text cannot be empty"], 400);
-        }
-        if(!is_null($request->files->get("file"))){
-            if($request->files->get("file")->getError() == 1){
-                return new JsonResponse(["error"=> "something went wrong with reading file! it might be corrupted"], 500);
+        if (!is_null($pageID)) {
+            $page = $this->getDoctrine()->getRepository(Page::class)->find(UUIDService::encodeUUID($pageID));
+            if (!$page) {
+                return new JsonResponse(["error" => "page with this id does not exist!"], 404);
             } else {
-                if(strpos($request->files->get("file")->getMimeType(), 'image') !== false || strpos($request->files->get("file")->getMimeType(), 'video') !== false){
-                    if($request->files->get("file")->getSize() < 20480000){
+                if ($page->getOwner()->getId() != UUIDService::encodeUUID($payload["user_id"])) {
+                    return new JsonResponse(["error" => "this page does not belong to you!"], 403);
+                }
+                $post->setPage($page);
+            }
+        }
+        if ($request->request->get("text") != "" && !is_null($request->request->get("text"))) {
+            $post->setText($request->request->get("text"));
+        } else {
+            return new JsonResponse(["error" => "text cannot be empty"], 400);
+        }
+        if (!is_null($request->files->get("file"))) {
+            if ($request->files->get("file")->getError() == 1) {
+                return new JsonResponse(["error" => "something went wrong with reading file! it might be corrupted"], 500);
+            } else {
+                if (strpos($request->files->get("file")->getMimeType(), 'image') !== false || strpos($request->files->get("file")->getMimeType(), 'video') !== false) {
+                    if ($request->files->get("file")->getSize() < 20480000) {
                         $post->setFileUrl($imageUploader->uploadFileToCloudinary($request->files->get("file")));
                     } else {
-                        return new JsonResponse(["error"=> "file is too big"], 400);
+                        return new JsonResponse(["error" => "file is too big"], 400);
                     }
                 } else {
-                    return new JsonResponse(["error"=> "wrong file type"], 400);
+                    return new JsonResponse(["error" => "wrong file type"], 400);
                 }
             }
         }
@@ -106,77 +143,81 @@ class PostController extends AbstractController
         $em = $this->getDoctrine()->getManager();
         $em->persist($post);
         $em->flush();
-        $resData = $serializer->serialize($post, "json",['ignored_attributes' => ['posts', "transitions", "password", "salt", "dateOfBirth", "roles", "email", "username","gender","post", "verified", "bannerUrl", "bio", "timezone"]]);
+        $resData = $serializer->serialize($post, "json", ['ignored_attributes' => ['posts', "transitions", "password", "salt", "dateOfBirth", "roles", "email", "username", "gender", "post", "verified", "bannerUrl", "bio", "timezone"]]);
         $tmp = json_decode($resData, true);
         $tmp["id"] = UUIDService::decodeUUID($tmp["id"]);
         $tmp["author"]["id"] = UUIDService::decodeUUID($tmp["author"]["id"]);
+        if (!is_null($tmp["page"])) {
+            $tmp["page"]["id"] = UUIDService::decodeUUID($tmp["page"]["id"]);
+            $tmp["page"]["owner"]["id"] = UUIDService::decodeUUID($tmp["page"]["owner"]["id"]);
+        }
         return new JsonResponse($tmp, 201);
     }
     /**
      * @Route("/{id}", name="edit_post", methods={"POST"})
      */
-    public function edit(Request $request, string $id,ImageUploader $imageUploader):JsonResponse
+    public function edit(Request $request, string $id, ImageUploader $imageUploader): JsonResponse
     {
 
         $payload = $request->attributes->get("payload");
         $em = $this->getDoctrine()->getManager();
         $post = $em->getRepository(Post::class)->find(UUIDService::encodeUUID($id));
-        if(!$post){
-            return new JsonResponse(["message"=>"Post does not exist"], 404);
+        if (!$post) {
+            return new JsonResponse(["error" => "Post does not exist"], 404);
         }
-        if($post->getAuthor()->getId() == UUIDService::encodeUUID($payload['user_id'])){
-            if($request->request->get("text") != "" && !is_null($request->request->get("text"))){
+        if ($post->getAuthor()->getId() == UUIDService::encodeUUID($payload['user_id'])) {
+            if ($request->request->get("text") != "" && !is_null($request->request->get("text"))) {
                 $post->setText($request->request->get("text"));
-            }else{
-                return new JsonResponse(["error"=>"text cannot be empty"], 400);
+            } else {
+                return new JsonResponse(["error" => "text cannot be empty"], 400);
             }
-            if(!is_null($request->files->get("file"))){
-                if($request->files->get("file")->getError() == 1){
-                    return new JsonResponse(["error"=> "something went wrong with reading file! it might be corrupted"], 500);
+            if (!is_null($request->files->get("file"))) {
+                if ($request->files->get("file")->getError() == 1) {
+                    return new JsonResponse(["error" => "something went wrong with reading file! it might be corrupted"], 500);
                 } else {
-                    if(strpos($request->files->get("file")->getMimeType(), 'image') !== false || strpos($request->files->get("file")->getMimeType(), 'video') !== false){
-                        if($request->files->get("file")->getSize() < 204800){
+                    if (strpos($request->files->get("file")->getMimeType(), 'image') !== false || strpos($request->files->get("file")->getMimeType(), 'video') !== false) {
+                        if ($request->files->get("file")->getSize() < 204800) {
                             $post->setFileUrl($imageUploader->uploadFileToCloudinary($request->files->get("file")));
                         } else {
-                            return new JsonResponse(["error"=> "file is too big"], 400);
+                            return new JsonResponse(["error" => "file is too big"], 400);
                         }
                     } else {
-                        return new JsonResponse(["error"=> "wrong file type"], 400);
+                        return new JsonResponse(["error" => "wrong file type"], 400);
                     }
                 }
-            }else{
+            } else {
                 $post->setFileUrl(null);
             }
             $post->setEditedAt(new DateTime("now"));
             $em->persist($post);
             $em->flush();
             $serializer = new Serializer([new ObjectNormalizer()], [new JsonEncoder()]);
-            $resData = $serializer->serialize($post, "json",['ignored_attributes' => ['posts', "transitions", "password", "salt", "dateOfBirth", "roles", "email", "username","gender","post", "verified", "bannerUrl", "bio", "timezone", "poComments"]]);
+            $resData = $serializer->serialize($post, "json", ['ignored_attributes' => ['posts', "transitions", "password", "salt", "dateOfBirth", "roles", "email", "username", "gender", "post", "verified", "bannerUrl", "bio", "timezone", "poComments"]]);
             $tmp = json_decode($resData, true);
             $tmp["id"] = UUIDService::decodeUUID($tmp["id"]);
             $tmp["author"]["id"] = UUIDService::decodeUUID($tmp["author"]["id"]);
-            return new JsonResponse(["message"=>"Post has been edited", "post"=> $tmp], 200);
+            return new JsonResponse(["message" => "Post has been edited", "post" => $tmp], 200);
         } else {
-            return new JsonResponse(["error"=>"This post does not belong to you"], 403);
+            return new JsonResponse(["error" => "This post does not belong to you"], 403);
         }
     }
     /**
      * @Route("/{id}", name="delete_post", methods={"DELETE"})
      */
-    public function remove(Request $request, string $id) :JsonResponse
+    public function remove(Request $request, string $id): JsonResponse
     {
         $payload = $request->attributes->get("payload");
         $em = $this->getDoctrine()->getManager();
         $post = $em->getRepository(Post::class)->find(UUIDService::encodeUUID($id));
-        if(!$post){
-            return new JsonResponse(["message"=>"Post does not exist"], 404);
+        if (!$post) {
+            return new JsonResponse(["error" => "Post does not exist"], 404);
         }
-        if($post->getAuthor()->getId() == UUIDService::encodeUUID($payload['user_id'])){
+        if ($post->getAuthor()->getId() == UUIDService::encodeUUID($payload['user_id'])) {
             $em->remove($post);
             $em->flush();
-            return new JsonResponse(["message"=>"Post has been deleted"], 200);
+            return new JsonResponse(["message" => "Post has been deleted"], 200);
         } else {
-            return new JsonResponse(["error"=>"This post does not belong to you"], 403);
-        }   
+            return new JsonResponse(["error" => "This post does not belong to you"], 403);
+        }
     }
 }
--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -20,9 +20,17 @@ class PostRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Post::class);
     }
-    public function createFindAllQuery(string $userID) : QueryBuilder
+    public function createFindAllQuery(string $userID): QueryBuilder
     {
-        return $this->createQueryBuilder("p")->leftJoin("App\Entity\Friend", "f", Join::WITH,"f.fr_friend=p.po_author")->where("f.fr_user = :id")->orderBy('p.po_created_at',"DESC")->setParameter("id", $userID);
+        return $this->createQueryBuilder("p")->leftJoin("App\Entity\Friend", "f", Join::WITH, "f.fr_friend=p.po_author")->where("f.fr_user = :id")->orderBy('p.po_created_at', "DESC")->setParameter("id", $userID);
+    }
+    public function createFindAllPostsForUser(string $userID): QueryBuilder
+    {
+        return $this->createQueryBuilder("p")->where("p.po_author = :id")->andWhere("p.po_page_id IS NULL")->orderBy('p.po_created_at', "DESC")->setParameter("id", $userID);
+    }
+    public function createFindAllPostsForPage(string $pageID): QueryBuilder
+    {
+        return $this->createQueryBuilder("p")->innerJoin("App\Entity\Page", "pa", Join::WITH, "p.po_page=pa.pa_id")->where("p.po_page = :id")->orderBy('p.po_created_at', "DESC")->setParameter("id", $pageID);
     }
     // /**
     //  * @return Post[] Returns an array of Post objects
